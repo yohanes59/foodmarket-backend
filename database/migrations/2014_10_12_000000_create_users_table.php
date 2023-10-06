c918e423fc618077e502f1ecdd9b43ae24a6f395
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-10-07 02:52:14 +0700

    feat:Create User Database Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,14 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->text('address')->nullable();
+            $table->text('houseNumber')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('city')->nullable();
+
+            $table->string('roles')->default('USER');
+            
             $table->timestamps();
         });
     }
