cbe2937423441d6addaf0b8618919b2e46a5bef7
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-10-09 02:14:54 +0700

    feat:Create route for UserController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +26,8 @@ Route::prefix('dashboard')
     ->middleware('auth:sanctum', 'admin')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('users', UserController::class);
     });
-
 
 // Midtrans related
 Route::get('midtrans/success', [MidtransController::class, 'success']);
