22cee331684890ff534c746e065de17a4129aeb8
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-10-09 20:51:44 +0700

    feat:Create route for transaction

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FoodController;
@@ -29,6 +30,7 @@ Route::prefix('dashboard')
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
         Route::resource('food', FoodController::class);
+        Route::resource('transaction', TransactionController::class);
     });
 
 // Midtrans related
